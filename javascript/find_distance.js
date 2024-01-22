29b7827d8e2191cd34026511eede9edc0e29381e
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2024-01-22 10:32:17 -0500

    done

--- javascript/find_distance.js
+++ javascript/find_distance.js
@@ -1,15 +1,34 @@
 function findDistance(graph, vertexA, vertexB) {
-  // type your code here
+  debugger;
+  let toVisit = [[vertexA, 0]];
+  const visited = new Set();
+
+  while (toVisit.length > 0) {
+    const [current, distance] = toVisit.shift();
+
+    if (current === vertexB && distance > 0) {
+      return distance;
+    }
+
+    if (!visited.has(current)) {
+      visited.add(current);
+      for (let vertex of graph[current]) {
+        toVisit.push([vertex, distance + 1]);
+      }
+    }
+  }
+
+  return -1;
 }
 
 if (require.main === module) {
   // add your own tests in here
   const graph = {
     jan: ["john", "jambaby"],
-    john: ["carl"],
+    john: ["carl", "jan"],
     jambaby: [],
     carl: ["jambaby"],
-    dave: []
+    dave: [],
   };
 
   console.log("Expecting: 2");
@@ -19,6 +38,16 @@ if (require.main === module) {
 
   console.log("Expecting: -1");
   console.log(findDistance(graph, "dave", "carl"));
+
+  console.log("");
+
+  console.log("Expecting: 3");
+  console.log(findDistance(graph, "jan", "jan"));
+
+  console.log("");
+
+  console.log("Expecting: 2");
+  console.log(findDistance(graph, "jan", "jambaby"));
 }
 
 module.exports = findDistance;
